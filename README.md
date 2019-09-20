d0a58d64762ca8c9e1695fac84b279f71adcc007
Thando <nodwengut@gmail.com>
2019-09-20 10:57:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/nodwengu/match-score-keeper.svg?branch=master)](https://travis-ci.org/nodwengu/match-score-keeper)
--- test/cricket-score-keeper-tests.js
+++ test/cricket-score-keeper-tests.js
@@ -2,9 +2,10 @@ const assert = require('assert');
 const CricketScoreKeeper = require('../cricketScoreKeeper');
 
 describe('Match Score Keeper Function ', () => {
+  const cricketScoreKeeper = CricketScoreKeeper();
   it('Should be able to fail', () => {
 
-    assert.deepEqual(settingsInstance.getActions(), result);
+    assert.equal(1, 2);
   })
 
   
